563b9681c81f7ff19bed751af0c78c38b26391bd
pydalwi <yodialwiono13@gmail.com>
2022-03-09 22:26:07 +0700

    Konversi_Suhu_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:suhu_konverter/convert.dart';
+import 'package:suhu_konverter/dropdown.dart';
+import 'package:suhu_konverter/input.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,147 +16,88 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   TextEditingController etInput = TextEditingController();
-
+  double _inputUser = 0;
   double _result = 0;
-  String selectedDropDown = "Kelvin";
-  List<String> listSuhu = [
-    "Kelvin",
-    "Reamur",
-    "Fahrenheit",
-  ];
-  List<String> listHasil = [];
+  String selectedDropdown = "Kelvin";
+  List<String> listSatuanSuhu = ["Kelvin", "Reamur"];
+
+  _konversiSuhu() {
+    setState(() {
+      if (etInput.text.isNotEmpty) {
+        _inputUser = double.parse(etInput.text);
+        switch (selectedDropdown) {
+          case "Kelvin":
+            {
+              _result = _inputUser + 273;
+            }
+            break;
+
+          case "Reamur":
+            {
+              _result = _inputUser * 0.8;
+            }
+            break;
+        }
+      }
+    });
+  }
+
+  _onDropdownChanged(Object? value) {
+    return setState(() {
+      selectedDropdown = value.toString();
+      _konversiSuhu();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Konverter Suhu',
+      title: "Flutter Demo",
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Konverter Suhu"),
+          title: const Text('Konverter Suhu'),
         ),
         body: Container(
           margin: const EdgeInsets.all(8),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              TextField(
-                controller: etInput,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                    hintText: "Masukkan Suhu Dalam Celcius"),
+              Input(etInput: etInput),
+              const SizedBox(
+                height: 10,
               ),
-              const SizedBox(height: 10),
-              DropdownButton(
-                isExpanded: true,
-                value: selectedDropDown,
-                items: listSuhu.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  onDropdownChanged(value);
-                },
+              Dropdown(
+                selectedDropdown: selectedDropdown,
+                listSatuanSuhu: listSatuanSuhu,
+                onChangedDropdown: _onDropdownChanged,
               ),
-              const SizedBox(height: 10),
+              const SizedBox(
+                height: 10,
+              ),
               const Text(
-                'Hasil',
-                style: TextStyle(fontSize: 23),
-              ),
-              Text(
-                '$_result',
-                style: const TextStyle(fontSize: 32),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        konversiSuhu();
-                      },
-                      child: const Text('Konversi Suhu'),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Riwayat Konversi',
+                "Hasil",
                 style: TextStyle(fontSize: 25),
               ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: ListView.builder(
-                    itemCount: listHasil.length,
-                    itemBuilder: (content, index) {
-                      return Text(listHasil[index]);
-                    }),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                "$_result",
+                style: const TextStyle(fontSize: 36),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Convert(
+                konvertHandler: _konversiSuhu,
               ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  void konversiSuhu() {
-    return setState(() {
-      if (etInput.text.isNotEmpty) {
-        switch (selectedDropDown) {
-          case "Kelvin":
-            _result = int.parse(etInput.text) + 273.15;
-            listHasil.add("Kelvin");
-            break;
-          case "Reamur":
-            _result = int.parse(etInput.text) * 0.8;
-            listHasil.add("Reamur");
-            break;
-          case "Fahrenheit":
-            _result = (int.parse(etInput.text) * 1.8) + 32;
-            listHasil.add("Fahrenheit");
-            break;
-        }
-        listHasil.add("Konversi dari " +
-            etInput.text +
-            " Celcius ke " +
-            selectedDropDown +
-            " dengan hasil = " +
-            _result.toString());
-      }
-    });
-  }
-
-  void onDropdownChanged(Object? value) {
-    return setState(() {
-      selectedDropDown = value.toString();
-      if (etInput.text.isNotEmpty) {
-        switch (selectedDropDown) {
-          case "Kelvin":
-            _result = int.parse(etInput.text) + 273.15;
-            listHasil.add("Kelvin");
-            break;
-          case "Reamur":
-            _result = int.parse(etInput.text) * 0.8;
-            listHasil.add("Reamur");
-            break;
-          case "Fahrenheit":
-            _result = (int.parse(etInput.text) * 1.8) + 32;
-            listHasil.add("Fahrenheit");
-            break;
-        }
-        listHasil.add("Konversi dari " +
-            etInput.text +
-            " Celcius ke " +
-            selectedDropDown +
-            " dengan hasil = " +
-            _result.toString());
-      }
-    });
   }
 }
